14205194056b22634b3c88c0556441eaf7884dce
Dekoder-py <kyleyesmariam@icloud.com>
2025-03-12 09:56:43 +1300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args){
+//        double kilometres = (100 * 1.6090344);
+//
+//        int highScore = 50;
+//
+//        if (highScore > 25) {
+//            highScore = 1000 + highScore;
+//        }
+//
+//        int health = 100;
+//
+//        if ((health < 25) && (highScore > 1000)) {
+//            highScore = highScore - 1000;
+//        }
+
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+
+        System.out.println("myVariable: " + myVariable);
+
+        if (myVariable == 0) {
+            System.out.println("It's now zero!");
+        }
+    }
+}
