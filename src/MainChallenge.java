dc3cb960dc5fc6c1290620252aecf929e2ccaf13
Dekoder-py <kyleyesmariam@icloud.com>
2025-03-12 10:43:37 +1300

    create a method to calculate the score

--- src/MainChallenge.java
+++ src/MainChallenge.java
@@ -8,19 +8,24 @@ public class MainChallenge {
 
         int finalScore = score;
 
-        if (gameOver) {
-            finalScore += (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-        }
+        calculateScore(gameOver, score, levelCompleted, bonus);
 
         score = 10_000;
         levelCompleted = 8;
         bonus = 200;
         finalScore = score;
 
+        calculateScore(gameOver, score, levelCompleted, bonus);
+
+    }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+
+        int finalScore = score;
+
         if (gameOver) {
             finalScore += (levelCompleted * bonus);
-            System.out.println("Your new final score was " + finalScore);
+            System.out.println("Your final score was " + finalScore);
         }
     }
 }
